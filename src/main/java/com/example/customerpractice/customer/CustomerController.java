1f40f748a0eb6d7d549e5e9f145a291d48756b6d
L <z0976117735@gmail.om>
2026-09-17 00:28:29 +0800

    feat: add customer controller

---
+++ src/main/java/com/example/customerpractice/customer/CustomerController.java
@@ -0,0 +1,62 @@
+package com.example.customerpractice.customer;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("api/v1/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerResponse create(@Valid @RequestBody CreateCustomerRequest request) {
+
+        Customer customer= customerService.create(
+                request.name(),
+                request.email(),
+                request.phone());
+
+        return CustomerResponse.from(customer);
+    }
+
+    @GetMapping("api/v1/customers")
+    public List<CustomerResponse> findAll() {
+        return customerService.findAll()
+                .stream()
+                .map(CustomerResponse::from)
+                .toList();
+    }
+
+    @GetMapping("api/v1/customers/{id}")
+    public CustomerResponse findById(@PathVariable Long id) {
+        Customer customer = customerService.findById(id);
+        return CustomerResponse.from(customer);
+    }
+
+    @PostMapping("api/v1/customers/{id}")
+    public CustomerResponse update(
+            @PathVariable Long id,
+            @Valid @RequestBody UpdateCustomerRequest request
+    ){
+           Customer customer= customerService.update(
+                   id,
+                   request.name(),
+                   request.email(),
+                   request.phone()
+           );
+           return CustomerResponse.from(customer);
+    }
+
+    @DeleteMapping("api/v1/customers/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        customerService.delete(id);
+    }
+}
