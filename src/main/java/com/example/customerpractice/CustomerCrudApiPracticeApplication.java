8d3081e6987380ebc460c140429fcae565dec9bc
L <z0976117735@gmail.om>
2026-09-16 20:42:39 +0800

    chore: initialize practice project

---
+++ src/main/java/com/example/customerpractice/CustomerCrudApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.customerpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCrudApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerCrudApiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customerpractice/CustomerCrudApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCrudApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
